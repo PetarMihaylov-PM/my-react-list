acda4b3a247c327e78e0d9bb314f9f591db49364
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-15 22:28:58 +0200

    Parent/Child Components

--- src/main.jsx
+++ src/main.jsx
@@ -10,22 +10,26 @@ root.render(
 function Page() {
   return (
     <>
-      <div>
-        <header>
-        <img src="src/assets/react.svg" alt="React logo" width='90px'/>
-        </header>
-        <main>
-          <h1>Why is React important</h1>
-          <ul>
-            <li>React is important for building fast, interactive UIs with reusable components.</li>
-            <li>It improves performance with a virtual DOM and simplifies scaling applications.</li>
-            <li>Its component-based structure aids maintainability.</li>
-          </ul>
-        </main>
-        <footer>
-          <small>© 2025 Mihaylov development. All rights reserved</small>
-        </footer>
-      </div>
+      <Header />
+      <main>
+         <h1>Why is React important</h1>
+         <ul>
+         <li>React is important for building fast, interactive UIs with reusable components.</li>
+         <li>It improves performance with a virtual DOM and simplifies scaling applications.</li>
+         <li>Its component-based structure aids maintainability.</li>
+         </ul>
+      </main>
+      <footer>
+         <small>© 2025 Mihaylov development. All rights reserved</small>
+      </footer>
     </>
   )
 }
+
+function Header() {
+   return (
+      <header>
+         <img src="src/assets/react.svg" alt="React logo" width='90px'/>
+      </header>
+   )
+} 
