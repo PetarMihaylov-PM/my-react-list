9df1e7ea04f80ca75575793a72ac5d2e1061258a
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-15 22:12:16 +0200

    learning components

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,21 @@ root.render(
 
 function Page() {
   return (
-    <header>
+    <div>
+      <header>
       <img src="src/assets/react.svg" alt="React logo" width='90px'/>
+      </header>
       <main>
-      <h1>Why is React important</h1>
-      <ul>
-        <li>React is important for building fast, interactive UIs with reusable components.</li>
-        <li>It improves performance with a virtual DOM and simplifies scaling applications.</li>
-        <li>Its component-based structure aids maintainability.</li>
-      </ul>
+        <h1>Why is React important</h1>
+        <ul>
+          <li>React is important for building fast, interactive UIs with reusable components.</li>
+          <li>It improves performance with a virtual DOM and simplifies scaling applications.</li>
+          <li>Its component-based structure aids maintainability.</li>
+        </ul>
       </main>
       <footer>
-        <p>© 2025 Mihaylov development. All rights reserved</p>
+        <small>© 2025 Mihaylov development. All rights reserved</small>
       </footer>
-    </header>
+    </div>
   )
 }
